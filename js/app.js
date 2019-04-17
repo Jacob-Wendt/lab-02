ee9560bff4d04b6ef1bfb6b31fa0c82f6410696e
Alexander <maniacallion@gmail.com>
2019-04-17 11:47:17 -0500

    rework of the js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 'use strict';
 
 $.get( './data/page-1.json', function( data ) {
-  // console.log(data[0].title);
-  // console.log(data[0].image_url);
   data.forEach(function(image) {
-    new Image(image); 
+    new Image(image);
   });
+}).done(function(){
+  Image.all.forEach(function(imagesOnPage){
+    imagesOnPage.renderImages();
+  })
 });
 
 
@@ -18,13 +20,14 @@ function Image (image_obj) {
   this.keyword = image_obj.keyword;
   this.horns = image_obj.horns;
   Image.all.push(this);
-  
+
 }
 console.log(Image.all);
 
-function renderImages() {
-   let imageTemplate =  $('#photo-template').clone();
-   imageTemplate.find('h2').text('hello world');
-}
+Image.prototype.renderImages = function() {
+  let imageTemplate =  $('#photo-template').clone();
+  imageTemplate.find('h2').text(this.title);
+  imageTemplate.appendTo('main');
 
-renderImages();
+};
+
